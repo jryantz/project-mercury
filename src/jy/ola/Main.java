6249be4076f76d68ddd7aba071d5c0840650887c
Jonathan Yantz <ijryantz@gmail.com>
2017-11-12 14:12:46 -0500

    added code to allow starting of server or client based on selection

--- src/jy/ola/Main.java
+++ src/jy/ola/Main.java
@@ -1,5 +1,8 @@
 package jy.ola;
 
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  *
  * @author jonathanyantz
@@ -9,7 +12,23 @@ public class Main {
 
     public Main() {
         
+        boolean cont = true;
         
+        while(cont) {
+            Scanner input = new Scanner(new InputStreamReader(System.in));
+            System.out.print("Server[s] / Client[c]: ");
+            String choice = input.nextLine();
+            
+            if(choice.equalsIgnoreCase("s")) {
+                Server s = new Server();
+                cont = false;
+            }
+                
+            if(choice.equalsIgnoreCase("c")) {
+                Client c = new Client();
+                cont = false;
+            }
+        }
         
     }//end Main
     

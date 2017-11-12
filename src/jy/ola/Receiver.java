281ed93e47037589984884a0992c340768579a95
Jonathan Yantz <ijryantz@gmail.com>
2017-11-12 13:01:49 -0500

    refactored and added for functionality in both client and server

--- src/jy/ola/Receiver.java
+++ src/jy/ola/Receiver.java
@@ -11,21 +11,34 @@ import java.net.DatagramPacket;
 public class Receiver implements Runnable {
     
     boolean running = true;
+    boolean server = false;
+    boolean client = false;
     
     @Override
     public void run() {
         
-        while(running) {
+        while(running && (server || client)) {
             byte[] receive = new byte[1024];
             
             try {
                 DatagramPacket receivePkt = new DatagramPacket(receive, receive.length);
-                Server.socket.receive(receivePkt);
                 
-                if(Server.remPort == 0) {
-                    System.out.println("\nClient connected!");
-                    Server.remPort = receivePkt.getPort();
-                    Server.remAddr = receivePkt.getAddress();
+                if(server) {
+                
+                    Server.socket.receive(receivePkt);
+
+                    if(Server.remPort == 0) {
+                        System.out.println("\nClient connected!");
+                        Server.remPort = receivePkt.getPort();
+                        Server.remAddr = receivePkt.getAddress();
+                    }
+                    
+                }
+                
+                if(client) {
+                    
+                    Client.socket.receive(receivePkt);
+                    
                 }
                 
                 String messageIn = new String(receivePkt.getData());

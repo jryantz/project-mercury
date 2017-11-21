c4456161fa039cd40d978d1231ef141f9101704d
Jonathan Yantz <ijryantz@gmail.com>
2017-11-20 23:31:57 -0500

    changed function names and added new

--- src/jy/main/Command.java
+++ src/jy/main/Command.java
@@ -2,6 +2,7 @@ package jy.main;
 
 import jy.ola.Packet;
 import jy.tools.Convert;
+import jy.tools.File;
 
 /**
  *
@@ -19,14 +20,21 @@ public class Command {
             args[1] = command.substring(command.indexOf(" ") + 1);
         } else {
             args[0] = command;
+            args[1] = "";
         }
         
         switch(args[0]) {
-            case "list":
+            case "list-file":
                 list();
+                break;
+            case "commands":
+                commands();
                 break;
             case "send":
                 send(args[1]);
+                break;
+            case "send-file":
+                
                 break;
             case "quit":
                 quit();
@@ -39,19 +47,34 @@ public class Command {
     
     private static void send(String packet) {
         
+        if(packet.length() <= 0) {
+            packet = "<no message>";
+        }
+        
         String binary = Convert.toBinary(packet.getBytes());
         
         if(binary.length() > 1024) {
             System.out.println("Message too large.");
         } else {
-            Main.node.send(Packet.single(binary));
+            Main.node.send(Packet.data(binary));
         }
         
     } // end send
     
     private static void list() {
         
-        System.out.println("list: list all command options");
+        String[] list = File.list();
+        
+        for(int i = 0; i < list.length; i++) {
+            System.out.println("[" + i + "]: " + list[i]);
+        }
+        
+    }
+    
+    private static void commands() {
+        
+        System.out.println("file-list: print a list of all files in the current directory");
+        System.out.println("commands: list all command options");
         System.out.println("send [String packet]: send a message with the node");
         System.out.println("quit: exit the program");
         
